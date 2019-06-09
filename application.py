7ee8f44567d5336db21b75d830ce4eb6eec31cd6
Derek <berkagmp@gmail.com>
2019-06-09 23:21:03 +1200

    Email Notification Test

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Awesome !!!</h1>
+    <h1>Awesome !!! Email Notification Test</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
